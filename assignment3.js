e6c5f21ccc0e535a3ea2a5812c4c9c33888ea8b3
Maryclare <mleonar8@nd.edu>
2022-01-28 17:22:49 -0500

    board displaying

--- assignment3.js
+++ assignment3.js
@@ -6,11 +6,18 @@ let ctx = canvas.getContext("2d"); ////store the 2d rendering context - tool use
 
 let fiveLetterWords = []
 
-brickHeight = 100;
-brickWidth = 100;
+brickHeight = 50;
+brickWidth = brickHeight;
+brickVerticalPadding = 10;
+brickHorizontalPadding = 10;
 
-boardBricksVertical = 6;
+
+boardBricksVertical = 5;
 boardBricksHorizontal = 6;
+boardWidth = (boardBricksHorizontal*brickWidth);
+boardXPadding = 25;
+boardYPadding = boardXPadding;
+numOfBricks = boardBricksVertical * boardBricksHorizontal;
 
 //create random int 
 function getRandomInt() {
@@ -23,7 +30,6 @@ function getRandomInt() {
 }
 
 //CLASS DECLARATIONS
-
 class Board {
     constructor(bricksVertical, bricksHorizontal, word, correctWord) {
         this._bricksVertical = bricksVertical;
@@ -31,79 +37,135 @@ class Board {
         this._word = word;
         this._correctWord = correctWord;
     }
+    //getters
+    getBricksVertical()     {   return this._bricksVertical; }
+    getBricksHorizontal()   {   return this._bricksHorizontal; }
+    getWord()               {   return this._word; }
+    getCorrectWord()        {   return this._correctWord; }
 
+    //setters
+    setBricksVertical(bricksVertical)       {   this._bricksVertical = bricksVertical;}
+    setBricksHorizontal(bricksHorizontal)   {   this._bricksHorizontal = bricksHorizontal;}
+    setWord(word)                           {   this._word = word; }
+    setCorrectWord(correctWord)             {   this._correctWord = correctWord; }
+}
+
+class Word {
+    constructor(letter1, correctLetter1, letter2, correctLetter2, letter3, correctLetter3, letter4, correctLetter4, letter5, correctLetter5) {
+        this._letter1 = letter1;
+        this._letter2 = letter2;
+        this._letter3 = letter3;
+        this._letter4 = letter4;
+        this._letter5 = letter5;
+        this._correctLetter1 = correctLetter1;
+        this._correctLetter2 = correctLetter2;
+        this._correctLetter3 = correctLetter3;
+        this._correctLetter4 = correctLetter4;
+        this._correctLetter5 = correctLetter5;
+    }
+    //getters
+    getLetter1()            {   return this._letter1; }
+    getLetter2()            {   return this._letter2; }
+    getLetter3()            {   return this._letter3; }
+    getLetter4()            {   return this._letter4; }
+    getLetter5()            {   return this._letter5; }
+    getCorrectLetter1()     {   return this._correctLetter1; }   
+    getCorrectLetter2()     {   return this._correctLetter2; }   
+    getCorrectLetter3()     {   return this._correctLetter3; }   
+    getCorrectLetter4()     {   return this._correctLetter4; }   
+    getCorrectLetter5()     {   return this._correctLetter5; }   
+
+    //setters
+    setLetter1(letter1)                 {   this._letter1 = letter1; }
+    setLetter2(letter2)                 {   this._letter2 = letter2; }
+    setLetter3(letter3)                 {   this._letter3 = letter3; }
+    setLetter4(letter4)                 {   this._letter4 = letter4; }
+    setLetter5(letter5)                 {   this._letter5 = letter5; }
+    setCorrectLetter1(correctLetter1)   {   this._correctLetter1 = correctLetter1;}
+    setCorrectLetter2(CorrectLetter2)   {   this._correctLetter2 = correctLetter2;}
+    setCorrectLetter3(correctLetter3)   {   this._correctLetter3 = correctLetter3;}
+    setCorrectLetter4(correctLetter4)   {   this._correctLetter4 = correctLetter4;}
+    setCorrectLetter5(correctLetter5)   {   this._correctLetter5 = correctLetter5;}
 }
 class Brick {
-    constructor(height,width,letter,color) {
+    constructor(height,width,letter,color,borderColor) {
         this._height = height;
         this._width = width;
         this._letter = letter;
         this._color = color;
+        this._borderColor = borderColor;
         //this.letter = "a";       //by default no text in brick
         //this.color = "##d9d9d9";
     }
     //getters
-    getHeight() {
-        return this._height;
-    }
-    getWidth() {
-        return this._width;
-    }
-    getLetter(){
-        return this._letter;
-    }
-    getColor() {
-        return this._color;
-    }
+    getHeight()         {   return this._height; }
+    getWidth()          {   return this._width; }
+    getLetter()         {   return this._letter; }
+    getColor()          {   return this._color; }
+    getBorderColor()    {   return this._borderColor; }  
     //setters
-    setLetter (letter) {
-        this._letter = letter;
-    }
-    setColor (color) {
-        this._color = color;
-    }
+    //don't need setter for Height or Width
+    setLetter (letter)          {   this._letter = letter; }
+    setColor (color)            {   this._color = color; }
+    setBorderColor(borderColor) {   this._borderColor = borderColor; }
 }
-/*
-//arrow key listeners for right goalie
-document.addEventListener("keydown", keyDownHandler, false);
 
-//stuck button and listener 
-const button = document.querySelector('#stuck');
-button.addEventListener('click', stuckButton);
-
-
-function keyDownHandler(e) {
-    if(e.key == "Up" || e.key == "ArrowUp"){
-        upPressed = true;
+//create board from words
+function createBoard() {
+    board = new Board();
+    //alert("here in createBoard");
+    for (let i =0; i < numOfBricks; i++ ){
+        brick = "bricks" + i;
+        iBrick = new Brick(brickHeight, brickWidth,"","#d9d9d9","#000000");
+        board[i] = iBrick;
     }
-    else if(e.key == "Down" || e.key == "ArrowDown"){
-        downPressed = true;
-    }
+
+    return board;
 }
-*/
-function drawBoard(brick1) {
-    ctx.beginPath();
-    //ctx.rect(10,10,10,10);
-    //ctx.fillStyle = "#F9B8";
-    //ctx.fill();
-    ctx.rect(10,10,brick1.getHeight(),brick1.getWidth());
-    ctx.fillStyle = brick1.getColor();
-    ctx.strokeStyle = "#000000";    
-    ctx.stroke();// add border
-    ctx.fill();
-    //ctx.fillStyle = "#FFFFFF";
-    //ctx.font = "15px Comic Sans MS";
-    //let scoreBoard = "  ScoreBoard  ";
-    //let joinedText = `${scoreLeft}${scoreBoard}${scoreRight}`
-    //ctx.fillText(brick1.getLetter(), 0, 0);
-    ctx.closePath(); 
+
+//create word from bricks
+function createWord() {
+    
+    
+    //return word;
+}
+
+function drawBoard(board) {
+    for (i=0; i < numOfBricks; i++){
+        brick = "brick" + i;
+        //alert(brick);
+        iBrick = board[i];
+        if (i < 5)          {   brickRow = 0; }
+        else if (i < 10)    {   brickRow = 1; }
+        else if (i < 15)    {   brickRow = 2; }
+        else if (i < 20)    {   brickRow = 3; }
+        else if (i < 25)    {   brickRow = 4; }
+        else if (i < 30)    {   brickRow = 5; }
+        iBrickXPos = boardXPadding + (i*(brickWidth+brickVerticalPadding)) - brickRow * boardWidth;
+        iBrickYPos = boardYPadding + (brickRow*(brickHeight+brickHorizontalPadding)); 
+        ctx.beginPath();
+        //ctx.rect(10,10,10,10);
+        //ctx.fillStyle = "#F9B8";
+        //ctx.fill();
+        ctx.rect(iBrickXPos,iBrickYPos,iBrick.getHeight(),iBrick.getWidth());
+        ctx.fillStyle = iBrick.getColor();
+        ctx.strokeStyle = iBrick.getBorderColor();    
+        ctx.stroke();// add border
+        ctx.fill();
+        //ctx.fillStyle = "#FFFFFF";
+        //ctx.font = "15px Comic Sans MS";
+        //let scoreBoard = "  ScoreBoard  ";
+        //let joinedText = `${scoreLeft}${scoreBoard}${scoreRight}`
+        //ctx.fillText(brick1.getLetter(), 0, 0);
+        ctx.closePath(); 
+    }
 }
 
 function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    const brick1 = new Brick(brickHeight,brickWidth,"m","#d9d9d9");
-    drawBoard(brick1);
+    
+    board =createBoard();
+    drawBoard(board);
 
     //requestAnimationFrame(draw); // function recalls itself infinitely
     //helps the browser render the game better than the fixed framerate we currently have implemented
